c18513b266de07147112871d9df3c3fa0429ec6f
Alspencer Omondi <alspenceramitojr@Alspencers-MacBook-Pro.local>
2023-02-11 10:02:33 -0800

    update the server url

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config();
 process.env.DEBUG = 'express:*';
@@ -17,35 +17,42 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    res.status(200).send({
-        message:'hello from Alspencer',
-    })
+// Add CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
-app.post('/', async(req, res) => {
-    try{
-        const prompt = req.body.prompt;
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'hello from Alspencer',
+  })
+});
 
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: prompt,
-            temperature: 0.7,
-            max_tokens: 1024,
-            top_p: 0.9,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-            // stop: "\n"  
-        });
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
 
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        })
-    } catch(error){
-        console.log(error);
-        res.status(50011).send({ error })
-    }
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.7,
+      max_tokens: 1024,
+      top_p: 0.9,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      // stop: "\n"  
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(50011).send({ error })
+  }
 })
 
 app.listen(50011, () => console.log('Server is running on port 50011'))
-
